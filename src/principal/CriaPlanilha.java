1703d1bba2a0a803e9ea912b2fd5f958a653cb93
Francisco Yury Pinheiro Silva <66030421+yury47@users.noreply.github.com>
2021-07-17 17:08:53 -0300

    17:08 tentativa de criacao de planilhas

---
+++ src/principal/CriaPlanilha.java
@@ -0,0 +1,38 @@
+package principal;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class CriaPlanilha{
+
+	@SuppressWarnings({ "unused", "resource" })
+	public static void criarPlanilha() throws IOException {
+		
+        // Creating Workbook instances
+        Workbook wb = new HSSFWorkbook();
+        //Workbook wb = new XSSFWorkbook();
+  
+        // An output stream accepts output bytes and sends them to sink.
+        OutputStream fileOut = new FileOutputStream("Carlos.xlsx");
+        //Prestar atenção na extensão do arquivo por conta da habilidade de leitura do MS Excel
+          
+        // Creating Sheets using sheet object
+        Sheet sheet1 = wb.createSheet("Array");
+        Sheet sheet2 = wb.createSheet("String");
+        Sheet sheet3 = wb.createSheet("Jabuti");
+        Sheet sheet4 = wb.createSheet("Tree");
+        Sheet sheet5 = wb.createSheet("Dynamic Programing");
+        Sheet sheet6 = wb.createSheet("Puzzles");
+          
+  
+        System.out.println("Sheets Has been Created successfully");
+  
+        wb.write(fileOut);
+	}
+}
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,31 +1,15 @@
 package principal;
 
-import java.io.*;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Principal {
 
-	public static void main(String[] args)throws FileNotFoundException, IOException {
-  
-        // Creating Workbook instances
-        Workbook wb = new HSSFWorkbook();
-  
-        // An output stream accepts output bytes and sends them to sink.
-        OutputStream fileOut = new FileOutputStream("Geeks_Jacare.xlsx");
-          
-        // Creating Sheets using sheet object
-        Sheet sheet1 = wb.createSheet("Array");
-        Sheet sheet2 = wb.createSheet("String");
-        Sheet sheet3 = wb.createSheet("LinkedList");
-        Sheet sheet4 = wb.createSheet("Tree");
-        Sheet sheet5 = wb.createSheet("Dynamic Programing");
-        Sheet sheet6 = wb.createSheet("Puzzles");
-          
-  
-        System.out.println("Sheets Has been Created successfully");
-  
-        wb.write(fileOut);
-    }
+	public static void main (String[] args) throws FileNotFoundException, IOException {
+		
+		CriaPlanilha.criarPlanilha();
+		
+	}
+		
 }
