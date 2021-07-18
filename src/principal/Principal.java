a6841a1f0c13966fc786db99ad2ae3f1991b5983
Francisco Yury Pinheiro Silva <66030421+yury47@users.noreply.github.com>
2021-07-17 23:48:08 -0300

    Última alteração: Criação da classe de leitura de JSON

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,13 +3,16 @@ package principal;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.json.simple.parser.ParseException;
+
 
 public class Principal {
 
-	public static void main (String[] args) throws FileNotFoundException, IOException {
+	public static void main (String[] args) throws FileNotFoundException, IOException, ParseException, java.text.ParseException {
 		
 		//CriaPlanilha.criarPlanilha();
-		TesteCriacaoPlanilha.criarPlanilhaPersonalizada();
+		//TesteCriacaoPlanilha.criarPlanilhaPersonalizada();
+		TesteLeituraJSON.lerJSON();
 	}
 		
 }
